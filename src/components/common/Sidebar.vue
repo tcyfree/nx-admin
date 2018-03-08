dc411dc6ae93349c29f883c332757e799fc6ce0d
tcyfree <1946644259@qq.com>
2018-03-08 14:08:21 +0800

    屏蔽一些测试页面

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -42,49 +42,49 @@
                                 index: 'user_list',
                                 title: '用户'
                             },
-                            {
-                                index: 'basetable',
-                                title: '基础表格'
-                            },
-                            {
-                                index: 'vuetable',
-                                title: 'Vue表格组件'
-                            }
+                            // {
+                            //     index: 'basetable',
+                            //     title: '基础表格'
+                            // },
+                            // {
+                            //     index: 'vuetable',
+                            //     title: 'Vue表格组件'
+                            // }
                         ]
                     },
-                    {
-                        icon: 'el-icon-date',
-                        index: '3',
-                        title: '表单',
-                        subs: [
-                            {
-                                index: 'baseform',
-                                title: '基本表单'
-                            },
-                            {
-                                index: 'vueeditor',
-                                title: '编辑器'
-                            },
-                            {
-                                index: 'markdown',
-                                title: 'markdown'
-                            },
-                            {
-                                index: 'upload',
-                                title: '文件上传'
-                            }
-                        ]
-                    },
-                    {
-                        icon: 'el-icon-star-on',
-                        index: 'basecharts',
-                        title: '图表'
-                    },
-                    {
-                        icon: 'el-icon-upload2',
-                        index: 'drag',
-                        title: '拖拽'
-                    }
+                    // {
+                    //     icon: 'el-icon-date',
+                    //     index: '3',
+                    //     title: '表单',
+                    //     subs: [
+                    //         {
+                    //             index: 'baseform',
+                    //             title: '基本表单'
+                    //         },
+                    //         {
+                    //             index: 'vueeditor',
+                    //             title: '编辑器'
+                    //         },
+                    //         {
+                    //             index: 'markdown',
+                    //             title: 'markdown'
+                    //         },
+                    //         {
+                    //             index: 'upload',
+                    //             title: '文件上传'
+                    //         }
+                    //     ]
+                    // },
+                    // {
+                    //     icon: 'el-icon-star-on',
+                    //     index: 'basecharts',
+                    //     title: '图表'
+                    // },
+                    // {
+                    //     icon: 'el-icon-upload2',
+                    //     index: 'drag',
+                    //     title: '拖拽'
+                    // }
                 ]
             }
         },
